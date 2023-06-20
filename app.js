e6a7be024823af33e0b6dbfe77c0d1b9d0123823
Creature Feature <ivy.mccurdy@gmail.com>
2023-06-19 22:26:55 -0500

    created if else statements to bring up employee, roles, and departments separetely when asked by the user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {
     createConnection 
 } = require('./config/dbConfig.js');
 
+const mysql= ('mysql2/promise');
 const inquirer= require('inquirer');
 
 async function main() {
@@ -18,15 +19,66 @@ async function main() {
         name: "menuOption",
         choices: ["View All Departments","View All Employees", "View All Roles","Add An Employee", "Add A Role", "Add A Department", "Update an Employee Role"],
     },
-   ]);
+   ],
+   );
 
-   if (selectedOption === "View All Departments") {
+   if (selectedOption.menuOption === "View All Departments") {
     try {
-        const result= await connection.execute (selectedOption);
-        console.log("YAY OMG JESUS CHRIST");
+        const [departments] = await connection.query('SELECT * FROM departments;');
+        console.table(departments);
     } catch (err) {
       console.error(err);
-    }
+    } 
+} else if (selectedOption.menuOption === "View All Roles") {
+    try {
+        const [roles] = await connection.query('SELECT * FROM roles JOIN departments ON roles.departments_id = departments.id;');
+        console.table(roles);
+    } catch (err) {
+      console.error(err);
+    } 
+} else if (selectedOption.menuOption === "View All Employees") {
+    try {
+        const [employees] = await connection.query('SELECT * FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.departments_id = departments.id;');
+        console.table(employees);
+        // console.log("YAY OMG JESUS CHRIST");
+    } catch (err) {
+      console.error(err);
+    }}
+    else if (selectedOption.menuOption === "Add A Department"){
+        try {    const newDepartment = await prompt([
+            {
+                type: "input",
+                name: "newDepartment",
+                message: 'Name of New Department',
+                default: null
+            },
+           ],
+           );
+        //    await mysql.createConnection(buildConnectionOptions());
+           await connection.query(
+            `INSERT INTO departments (name) VALUES ('${newDepartment}');`
+          );
+          const [updated] = await connection.query("SELECT * FROM departments;");
+          console.table(updated);
+
+        }catch (err) {
+            console.error(err);
+    } 
+}}
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //    console.log(selectedOption);
   
@@ -38,6 +90,3 @@ async function main() {
 
 //    const [employees] =await connection.execute('SELECT * FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.departments_id = departments.id;', []);
 //    console.table(employees);
-}
-}
-main();
